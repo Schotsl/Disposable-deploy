fcb1fc5b40abe79b2eb1e77e556be20cfa7c1b69
GitHub <noreply@github.com>
2022-11-18 00:21:36 +0000

    ⬆️ Update dependencies

--- entity/ImageEntity.ts
+++ entity/ImageEntity.ts
@@ -1,9 +1,9 @@
-import BaseEntity from "https://raw.githubusercontent.com/Schotsl/Uberdeno/v1.0.1/entity/BaseEntity.ts";
+import BaseEntity from "https://raw.githubusercontent.com/Schotsl/Uberdeno/v1.0.2/entity/BaseEntity.ts";
 
 import {
   IntColumn,
   SmallColumn,
-} from "https://raw.githubusercontent.com/Schotsl/Uberdeno/v1.0.1/other/Columns.ts";
+} from "https://raw.githubusercontent.com/Schotsl/Uberdeno/v1.0.2/other/Columns.ts";
 
 export default class ImageEntity extends BaseEntity {
   public size = new IntColumn("size");
