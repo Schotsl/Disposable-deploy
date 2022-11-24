2a2295ee2e0009413e231c3b6f6380cb4dd9ba86
GitHub <noreply@github.com>
2022-11-24 00:19:42 +0000

    ⬆️ Update dependencies

--- collection/ImageCollection.ts
+++ collection/ImageCollection.ts
@@ -1,5 +1,5 @@
 import ImageEntity from "../entity/ImageEntity.ts";
-import BaseCollection from "https://raw.githubusercontent.com/Schotsl/Uberdeno/v1.0.3/collection/BaseCollection.ts";
+import BaseCollection from "https://raw.githubusercontent.com/Schotsl/Uberdeno/v1.0.4/collection/BaseCollection.ts";
 
 export default class ImageCollection extends BaseCollection {
   public images: ImageEntity[] = [];
